98a561337c30c719c25a9f1dda152f070316d5fd
AnneGicquel <annelaedere@gmail.com>
2023-02-11 21:39:32 +0100

    button fait

---
+++ src/app/components/selection-button/selection-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selection-button',
+  templateUrl: './selection-button.component.html',
+  styleUrls: ['./selection-button.component.css']
+})
+export class SelectionButtonComponent {
+
+}
