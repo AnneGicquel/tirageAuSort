5a3c17931db5468771c71c63708b159e4429c40e
AnneGicquel <annelaedere@gmail.com>
2023-02-11 23:02:53 +0100

    Ajouter les absent.e.s fait

---
+++ src/app/components/add-absent/add-absent.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddAbsentComponent } from './add-absent.component';
+
+describe('AddAbsentComponent', () => {
+  let component: AddAbsentComponent;
+  let fixture: ComponentFixture<AddAbsentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddAbsentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddAbsentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
